43adaa8542abe21a7a534d6625d92a6e0c7edfcb
Rinrealvngangz <tuannmctk42@gmail.com>
2021-03-13 11:07:22 +0700

    add all

--- eBookShop/app/Http/Controllers/UserController.php
+++ eBookShop/app/Http/Controllers/UserController.php
@@ -1,12 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Http\Requests\CreateUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use App\Models\Photo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Spatie\Permission\Traits\HasRoles;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     use HasRoles;
@@ -35,7 +38,7 @@ class UserController extends Controller
      */
     public function create()
     {
-        //
+         return view('admin.user.create');
     }
 
     /**
@@ -44,9 +47,16 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store( CreateUserRequest $request)
     {
-        //
+         User::create([
+            'firstName' => $request['firstName'],
+            'lastName' => $request['lastName'],
+            'userName' => $request['userName'],
+            'email' => $request['email'],
+            'password' => Hash::make($request['password']),
+        ]);
+        return redirect()->route('user.index');
     }
 
     /**
@@ -90,19 +100,26 @@ class UserController extends Controller
              $name = time() . $file->getClientOriginalName();
              $file->move('images',$name);
              $photo = Photo::create(['file'=>$name]);
-
               $input['photo_id'] = $photo->id;
 
          }
 
-       $input['password'] = bcrypt($request->password);
+        $input['password'] = bcrypt($request->password);
          $user->firstName = $request->input('firstName');
          $user->lastName = $request->input('lastName');
          $user->userName =$request->input('userName');
-         $user->email = $request->input('email');
+          $email = User::all()->where('email',$input['email'])->first();
+          if($email === $user->email &&$email !==null ||$email ===null ){
+              $user->email = $request->input('email');
+          }
          $user->password =$input['password'];
-         $user->photo_id =$input['photo_id'];
-         $user->save();
+        if(array_key_exists('photo_id',$input)){
+            $user->photo_id =$input['photo_id'];
+        }
+
+        $user->save();
+
+        $request->session()->flash('update-user','User update success!');
          return  redirect()->route('user.index');
     }
 
@@ -114,6 +131,62 @@ class UserController extends Controller
      */
     public function destroy($id)
     {
+        $user = User::findOrFail($id);
 
+       if($user->photo){
+           unlink( public_path() . $user->photo->file);
+           $user->photo->delete();
+       }
+        $user::destroy($id);
+
+       session()->flash('delete-user','Delete  user success!');
+        return redirect()->route('user.index');
+    }
+
+    public function editRole($id){
+
+        $user = User::findOrFail($id);
+        $role = Role::all();
+        $arrayIdRole = array();
+        $IdRole =$user->roles;
+        foreach ($IdRole as $idRoles){
+            array_push($arrayIdRole,$idRoles->id);
+        }
+        return view('admin.user.user_role',compact('role','arrayIdRole','user'));
+    }
+
+    /**
+     * add role user
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function addRole( Request $request,$id){
+
+        $user = User::findOrFail($id);
+        $input = $request->all();
+        $arrayNameRole = array();
+        $originRole = Role::all();
+
+        foreach ($originRole as $nameRole){
+            array_push($arrayNameRole,$nameRole->name);
+        }
+
+
+            if(array_key_exists('arrayIdRole',$input))
+            {
+                foreach ($originRole as $role){
+                    if(in_array($role,$input['arrayIdRole']) == false){
+
+                        $user->removeRole($role);
+                    }
+                }
+                $user->assignRole($input['arrayIdRole']);
+            }else{
+                $user->syncRoles([]);
+            }
+        $request->session()->flash('assignRole-user','Assign role user success!');
+           return redirect()->route('user.index');
     }
 }
